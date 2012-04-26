9e02b76e131b3e4d1d6a705156cd666e2d6bc2f0
Spiros Ioannou <sivann@mute>
2012-04-26 23:51:44 +0300

    lastchanges

--- contrib/mkpackage.sh
+++ contrib/mkpackage.sh
@@ -2,11 +2,12 @@
 
 #don't run this yourselves
 
-version=`grep version ../itdb.php |head -1|cut -d'"' -f2`
+version=`grep version ../index.php |head -1|cut -d'"' -f2`
 echo "updating version on database to $version"
 echo "update settings set version='$version';"|sqlite3 ../data/itdb.db 
 echo "update settings set version='$version';"|sqlite3 ../data/pure.db 
 
+echo "Copying"
 rm -fr /tmp/itdb
 cp -a ../../itdb /tmp
 cd /tmp #just to be sure we are not someplace dangerous
@@ -15,10 +16,12 @@ cd translations
 cat el.txt |cut -d'#' -f1 |sed 's/$/#/'>new.txt 
 cd ..
 rm -f data/files/*
+rm -f .git/
 rm -f images/eoa*
 rm data/itdb.db
-#cp conf.php.sample conf.php
+
 cd /tmp
+
 echo "it is ready for tarring in /tmp/itdb"
 
-echo "you may do a  tar zcvf itdb-${version}.tar.gz itdb/ in there"
+echo "you may do a  tar zcvf itdb-${version}.tar.gz itdb/ "

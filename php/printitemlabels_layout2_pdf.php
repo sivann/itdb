8c5479b558dde5c896f941d9ec510fa5a316b19e
Chefkeks <chefkeks@googlemail.com>
2014-10-15 16:07:39 +0200

    New layout and barcode layout for label printing
    Added one normal new layout and a new barcode layout for ITDB. We used
    them with Avery Zweckform sheets and they work really good.
    
    Below some screenshots of how the labels will look and what changes at
    the interface. Please ignore the borders around the labels ... had not
    the time to make it fit into the label, so I just used the existing
    labels without any modification which are obviously not suitable with
    adjustment.

---
+++ php/printitemlabels_layout2_pdf.php
@@ -0,0 +1,184 @@
+<?php 
+//error_reporting(E_ALL); 
+/* Spiros Ioannou 2009 , sivann _at_ gmail.com */
+
+require("../init.php");
+require_once('PDF_Label_layout2.php');
+
+
+set_time_limit(15);
+
+
+//create the list of previously selected ids to get info from db
+$ids="";
+for ($i=0;$i<count($selitems);$i++)  {
+  $ids.="'".$selitems[$i]."'";
+  if ($i<count($selitems)-1) $ids.=", ";
+}
+
+
+
+
+//$sql="SELECT items.id,model,sn,sn3,itemtypeid,dnsname,ipv4,ipv6,label, agents.title as agtitle FROM items,agents ".
+//     " WHERE agents.id=items.manufacturerid AND items.id in ($ids) order by itemtypeid, agtitle, model,sn,sn2,sn3";
+//$sql="SELECT items.id,model,sn,sn3,itemtypeid,dnsname,ipv4,ipv6,label, agents.title as agtitle FROM items,agents ".
+//     " WHERE agents.id=items.manufacturerid AND items.id in ($ids) order by items.id";
+$sql="SELECT items.id,model,sn,sn2,sn3,itemtypeid,dnsname,ipv4,ipv6,label, agents.title as agtitle FROM items,agents ".
+     " WHERE agents.id=items.manufacturerid AND items.id in ($ids) order by items.id";
+$sth=db_execute($dbh,$sql);
+$idx=0;
+
+
+
+$pdf=new PDF_Label(array(
+  'paper-size'=>"$labelpapersize", 
+  'metric'=>'mm',
+  'marginLeft'=>$lmargin,
+  'marginTop'=>$tmargin,
+  'NX'=>$cols,
+  'NY'=>$rows,
+  'SpaceX'=>($hpitch-$lwidth),
+  'SpaceY'=>($vpitch-$lheight),
+  'width'=>$lwidth,
+  'height'=>$lheight,
+  'font-size'=>$fontsize));
+
+$pdf->AddPage();
+$pdf->SetAuthor('ITDB Asset Management');
+$pdf->SetTitle('Items');
+
+$pdf->setFontSubsetting(true);
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+
+/* skip specified labels, to avoid printing on missing label positions on reused papers*/
+for ($skipno=0;$skipno<$labelskip;$skipno++) {
+  $pdf->Add_Label("","",0,255,"",0,0,6,6);
+}
+
+$pages=0;
+
+for ($row=1;$row<=$rows;$row++) {
+  if ($pages>30) break;
+  for ($col=1;$col<=$cols;$col++) {
+
+    $r=$sth->fetch(PDO::FETCH_ASSOC);
+    if (!$r) break;
+
+    $idesc=$itypes[$r['itemtypeid']]['typedesc'];
+    $id=sprintf("%04d",$r['id']);
+    $dnsname=$r['dnsname'];
+    $ipv4=$r['ipv4'];
+    $ipv4=mb_substr($ipv4,0,15);
+    $ipv6=$r['ipv6'];
+    $agtitle=$r['agtitle'];
+    $model=$r['model'];
+    $label=$r['label'];
+
+    //$desc="$agtitle/$model";
+    //$desc=mb_substr($desc,0,37);
+
+    // Trim after 20 character to fit Avery Zweckform ZWE3651
+    $desc="$agtitle $model";
+    $desc=mb_substr($desc,0,20);
+    $desc=trim($desc);
+    //$sn=strlen($r['sn'])>0?$r['sn']:$r['sn3'];
+	
+	$sn=strlen($r['sn'])>0?$r['sn']:$r['sn'];
+    $sn2=strlen($r['sn2'])>0?$r['sn2']:$r['sn2'];
+    $sn3=strlen($r['sn3'])>0?$r['sn3']:$r['sn3'];
+
+// Original Label Layout Start
+/*    
+    $labeltext="";
+    $labeltext.=sprintf("ID:$id\n");
+    if (strlen($label)) $labeltext.=sprintf("LBL:$label\n");
+
+    if (strlen($sn)) $labeltext.=sprintf("SN:$sn\n");
+
+    if (strlen($desc)) { $labeltext.=$desc."\n"; }
+
+    if (strlen($ipv4)) { $labeltext.="IPv4:$ipv4\n"; }
+    if (strlen($ipv6)) { $labeltext.="IPv6:$ipv6\n"; }
+
+    if (strlen($dnsname)) { 
+       $labeltext.="HName:$dnsname\n";
+    }
+    $labeltext=rtrim($labeltext);
+*/
+// Original Label Layout End
+
+// New Layout:
+	$labeltext="";
+    $labeltext.=sprintf("ID:$id\n");
+    if (strlen($desc)) { $labeltext.="Model: ".$desc."\n"; }
+    else $labeltext.=sprintf("Model: \n");
+    if (strlen($sn)) { $labeltext.=sprintf("SN: $sn\n\n"); }
+    else $labeltext.=sprintf("SN: \n\n");
+    if (strlen($sn2)) { $labeltext.=sprintf("Inventory:\n$sn2\n"); }
+    else $labeltext.=sprintf("Inventory: \n\n");
+    if (strlen($sn3)) { $labeltext.=sprintf("$sn3\n"); }
+    else $labeltext.=sprintf("\n");
+    $labeltext=rtrim($labeltext);
+
+
+    if (!$wantheadertext)
+      $headertext="";
+
+    if (!$wantheaderimage) 
+      $image="";
+
+    if ($wantbarcode) {
+      $barcode=$qrtext.$id;
+      //$barcode = mb_strtoupper($barcode, 'UTF-8');
+    }
+    else {
+      $barcode="";
+	}
+
+    $headertext=str_replace('_NL_',"\n",$headertext);
+
+    $nbw=0.30; //code39 narrow bar width (mm). 15+1 narrow bars/character (+1=spacing)
+    $barcodewidth=((strlen($barcode)+3)*(15+2)+20)*$nbw;
+    //code39:bh:(mm) barcode height must allow for 15 degrees of scanning ideally
+    //code39:includes barcode text font width (about 3mm)
+    $bh=max(0.15*$barcodewidth+3,16); //at least 15% of length
+    
+
+    $pdf->Add_Label($headertext,$labeltext,$padding,$border,
+                    $image,$imagewidth,$imageheight,
+                    $headerfontsize,$fontsize,$idfontsize,
+		    $barcode, $nbw,$bh,$barcodesize );
+
+
+    // for code39 barcodes: check if barcode fits inside label, allowing space for its quiet zone
+    //2 narrow spacings if nbw>0.29, + 1 char checksum +2 chars start/stop (*) +10 bars quiet zone on each side
+/*
+    if ($barcodewidth>=$lwidth) {
+      $pdf->Ln();
+      $pdf->Cell(0,20,"Required barcode width ($barcodewidth mm) too wide for label width ($lwidth mm)",1,1,'C');
+      break;
+
+    }
+*/
+  }
+
+  if (($row==$rows)) {
+    $row=0;
+    $col=0;
+    $pages++;
+    //$pdf->AddPage('P','A4');
+  }
+
+
+}
+
+$dstr=date('Y-m-d_his');
+$pdf->Output("labels-$dstr.pdf",'D');
+
+?>
+
+
+
+</body>
+</html>

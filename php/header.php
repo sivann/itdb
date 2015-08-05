b26de4a08a0cf0703d7b70dc16d7c7c69b129197
Chefkeks <chefkeks@googlemail.com>
2015-08-05 09:53:35 +0200

    OOPS: Fix issues in logic and added check if itdb.css AND theme.css exist in theme folder

--- php/header.php
+++ php/header.php
@@ -13,13 +13,21 @@
 <link type="text/css" href="css/jquery-themes/blue2/jquery-ui-1.8.12.custom.css" rel="stylesheet" >
 
 <?php
-if (($authstatus) && ($theme != "default" ) && (file_exists("css/itdb-themes/".$theme."/itdb.css")))
+if ($authstatus)
 {
     $theme = theme();
+    if (($theme != "default" ) && (file_exists("css/itdb-themes/".$theme."/itdb.css")) && (file_exists("css/itdb-themes/".$theme."/theme.css")))
+    {
+        ?>
+        <link rel="stylesheet" href="css/itdb-themes/<?php echo $theme; ?>/itdb.css" type="text/css">
+        <link rel="stylesheet" href="css/itdb-themes/<?php echo $theme; ?>/theme.css" type="text/css">
+        <?php
+    }  else {
     ?>
-    <link rel="stylesheet" href="css/itdb-themes/<?php echo $theme; ?>/itdb.css" type="text/css">
-    <link rel="stylesheet" href="css/itdb-themes/<?php echo $theme; ?>/theme.css" type="text/css">
+    <link rel="stylesheet" href="css/itdb.css" type="text/css">
+    <link rel="stylesheet" href="css/theme.css" type="text/css">
     <?php
+    }
 } else {
     ?>
     <link rel="stylesheet" href="css/itdb.css" type="text/css">

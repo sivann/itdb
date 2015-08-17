85ce4817dbb90ba35a164049349f1ef0f98a1649
Chefkeks <chefkeks@googlemail.com>
2015-08-17 10:05:06 +0200

    NEW: Added database update file and db version 7 in index file

---
+++ updates/db/6_7.sql
@@ -0,0 +1,7 @@
+begin transaction;
+alter table settings add column usedns integer default 0;
+alter table settings add column dns_servers;
+alter table settings add column dns_suffix;
+alter table settings add column dns_autoupdate integer default 0;
+update settings set dbversion=7;
+commit;

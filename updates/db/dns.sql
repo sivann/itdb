804b4b23512b310151e615757069950579c070c0
Michael <Chefkeks@users.noreply.github.com>
2015-09-03 14:43:19 +0200

    Update and rename 6_7.sql to dns.sql

--- updates/db/dns.sql
+++ updates/db/dns.sql
@@ -3,5 +3,4 @@ alter table settings add column usedns integer default 0;
 alter table settings add column dns_servers;
 alter table settings add column dns_suffix;
 alter table settings add column dns_autoupdate integer default 0;
-update settings set dbversion=7;
 commit;
